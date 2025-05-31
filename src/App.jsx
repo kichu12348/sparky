89d06ab644f749355886657cb96cf06fd952fe18
Mahadevan Reji <rmahadevan574@gmail.com>
2025-05-31 21:06:53 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import "./App.css";
+import { useState } from "react";
+
+function Model() {
+  const { scene } = useGLTF("/models/sparky_glb.glb");
+  const [rotateY, setRotateY] = useState(-Math.PI / 2);
+
+  useFrame(()=>{
+    setRotateY((prev) => prev + 0.01);
+    scene.rotation.y = rotateY;
+  })
+  return (
+    <mesh>
+      <OrbitControls
+        enableZoom={true}
+        enablePan={true}
+        enableRotate={true}
+        minDistance={1}
+        maxDistance={5}
+        target={[0, 0, 0]}
+        />
+      <primitive
+        object={scene}
+        scale={1}
+        position={[0, 0, 0]}
+        rotation={[0, -Math.PI / 2, 0]}
+        castShadow
+        autoRotate
+        autoRotateSpeed={2}
+      />
+    </mesh>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Canvas
+        style={{ height: "100svh", width: "100vw" }}
+        camera={{ position: [0, 0, 2], fov: 75 }}
+        shadows
+      >
+        <ambientLight intensity={10} />
+        <directionalLight position={[5, 5, 5]} intensity={5} castShadow />
+
+        <Model />
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
