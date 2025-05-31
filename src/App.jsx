cbb0a8b15cef605379fe5dc8caf95642fe52e59d
Mahadevan Reji <rmahadevan574@gmail.com>
2025-05-31 21:15:42 +0530

    Refactor App component to include Suspense for model loading and improve code formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import "./App.css";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 
 function Model() {
   const { scene } = useGLTF("/models/sparky_glb.glb");
   const [rotateY, setRotateY] = useState(-Math.PI / 2);
 
-  useFrame(()=>{
+  useFrame(() => {
     setRotateY((prev) => prev + 0.01);
     scene.rotation.y = rotateY;
-  })
+  });
   return (
     <mesh>
       <OrbitControls
@@ -20,7 +20,7 @@ function Model() {
         minDistance={1}
         maxDistance={5}
         target={[0, 0, 0]}
-        />
+      />
       <primitive
         object={scene}
         scale={1}
@@ -34,19 +34,47 @@ function Model() {
   );
 }
 
+const FallBack = () => {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+
+        "h1": {
+          fontSize: "2rem",
+          color: "#fff",
+        },
+        "p": {
+          fontSize: "1rem",
+          color: "#ccc",
+        },
+      }}
+    >
+      <h1>Loading...</h1>
+      <p>Please wait while the model is loading.</p>
+    </div>
+  );
+};
+
 function App() {
   return (
     <>
-      <Canvas
-        style={{ height: "100svh", width: "100vw" }}
-        camera={{ position: [0, 0, 2], fov: 75 }}
-        shadows
-      >
-        <ambientLight intensity={10} />
-        <directionalLight position={[5, 5, 5]} intensity={5} castShadow />
+      <Suspense fallback={<FallBack />}>
+        <Canvas
+          style={{ height: "100svh", width: "100vw" }}
+          camera={{ position: [0, 0, 2], fov: 75 }}
+          shadows
+        >
+          <ambientLight intensity={10} />
+          <directionalLight position={[5, 5, 5]} intensity={5} castShadow />
 
-        <Model />
-      </Canvas>
+          <Model />
+        </Canvas>
+      </Suspense>
     </>
   );
 }
